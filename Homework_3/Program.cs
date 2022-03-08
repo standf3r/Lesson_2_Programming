4bc396daf8a9b3a0a8af3d078c302778c9fb46e4
standfer <vertigl@yandex.ru>
2022-03-08 19:04:56 +0800

    Homework 3: checking for multiplicity

---
+++ Homework_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить, кратно ли часло заданному, если нет, вывести остаток
+
+
+Console.Write("Введите первое число: ");
+int a  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b  = Convert.ToInt32(Console.ReadLine());
+int c = (a % b);
+int d = (a / b);
+if (a%b==0)
+{
+    Console.WriteLine($"Число кратно заданному, без остатка равен: {d}");
+}
+else
+{
+    Console.WriteLine($"Заданное число делится с остатком, остаток равен: {c}");
+}
+
